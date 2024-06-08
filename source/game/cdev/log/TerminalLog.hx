c2186a62fbf6c7c793ce74419efc94f02576d6c9
Cobalt Bar <79053181+CobaltBar@users.noreply.github.com>
2024-06-08 10:28:10 -0700

    new logging format

--- source/game/cdev/log/TerminalLog.hx
+++ source/game/cdev/log/TerminalLog.hx
@@ -4,9 +4,12 @@ package game.cdev.log;
  * Used for colored traces in Terminal
  * Written by Cobalt Bar - https://github.com/CobaltBar/FNF-Horizon-Engine/blob/main/source/util/Log.hx
  * Modified by CoreCat/Dev for use in CDEV Engine
+ *
+ * :3
  */
-
+import haxe.Constraints.Function;
 import haxe.PosInfos;
+
 using StringTools;
 
 enum abstract AnsiMode(Int)
@@ -47,71 +50,93 @@ class TerminalLog
 {
 	// https://gist.github.com/martinwells/5980517
 	// https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797
-	private static var ogTrace:Dynamic;
+	private static var ogTrace:Function;
+	private static var log:String = "";
 
+	/*
+		if (!FileSystem.exists('log'))
+			FileSystem.createDirectory('log');
+		Log.info('Log Written');
+		File.saveContent('log/log.txt', @:privateAccess Log.log);
+
+
+		this saves the log to `log/log.txt`
+	 */
 	public static function init():Void
 	{
 		ogTrace = haxe.Log.trace;
 		haxe.Log.trace = haxeTrace;
-		prettyPrint("CDEV Engine v"+CDevConfig.engineVersion+"\n(Logging style written by CobaltBar!)");
+		prettyPrint("CDEV Engine v" + CDevConfig.engineVersion + "\n(Logging style written by CobaltBar!)");
 		info('Logger Initialized');
 	}
 
-	@:keep public static inline function ansiColor(mode:AnsiMode, color:AnsiColor):String
+	@:keep public static inline function ansi(mode:AnsiMode, color:AnsiColor):String
 		return '\033[${mode};${color}m';
 
-	static function haxeTrace(value:Dynamic, ?pos:PosInfos):Void
+	@:keep static inline function haxeTrace(value:Dynamic, ?pos:PosInfos):Void
 		print(value, 'TRACE', BOLD, HIGHINTENSITY_BLUE, pos);
 
-	public static function error(value:Dynamic, ?pos:PosInfos):Void
+	@:keep public static inline function error(value:Dynamic, ?pos:PosInfos):Void
 		print(value, 'ERROR', BOLD, RED, pos);
 
-	public static function warn(value:Dynamic, ?pos:PosInfos):Void
+	@:keep public static inline function warn(value:Dynamic, ?pos:PosInfos):Void
 		print(value, 'WARN', BOLD, YELLOW, pos);
 
-	public static function info(value:Dynamic, ?pos:PosInfos):Void
+	@:keep public static inline function info(value:Dynamic, ?pos:PosInfos):Void
 		print(value, 'INFO', BOLD, CYAN, pos);
 
-	public static function script(value:Dynamic, ?pos:PosInfos):Void
+	@:keep public static inline function script(value:Dynamic, ?pos:PosInfos):Void
 		print(value, 'SCRIPT', BOLD, MAGENTA, pos);
 
-	static function print(value:Dynamic, level:String, mode:AnsiMode, color:AnsiColor, ?pos:PosInfos):Void
+	@:keep static inline function print(value:Dynamic, level:String, mode:AnsiMode, color:AnsiColor, ?pos:PosInfos):Void
 	{
-		var msg:String = '${ansiColor(RESET, BLUE)}[${ansiColor(RESET, CYAN)}${DateTools.format(Date.now(), '%H:%M:%S')}${ansiColor(RESET, BLUE)}]';
+		var msg:String = '${ansi(RESET, BLUE)}[${ansi(RESET, CYAN)}${DateTools.format(Date.now(), '%H:%M:%S')}';
 		if (pos != null)
-			msg += '[${ansiColor(BOLD, WHITE)}${pos.fileName.replace("source/","")}:${pos.lineNumber}${ansiColor(RESET, BLUE)}]';
-		msg += [for (i in 0...90 - msg.length) ' '].join('');
-		msg += '[${ansiColor(mode, color)}${level}${ansiColor(RESET, BLUE)}]${ansiColor(mode, color)}:   ';
-		msg += '${ansiColor(RESET, color)}$value${ansiColor(RESET, RESET)}';
+			msg += '${ansi(RESET, BLUE)} - ${ansi(BOLD, WHITE)}${pos.fileName.replace("source/", "")}:${pos.lineNumber}';
+		msg += '${ansi(RESET, BLUE)}]';
+		msg = (msg.rpad(' ', 90) + '${ansi(mode, color)}${level}: ').rpad(' ', 95) + '${ansi(RESET, color)}$value${ansi(RESET, RESET)}';
 		Sys.println(msg);
+
+		var logMsg:String = '[${DateTools.format(Date.now(), '%H:%M:%S')}';
+		if (pos != null)
+			logMsg += ' - ${pos.fileName}:${pos.lineNumber}';
+		logMsg += '] ${level}: $value\n';
+		log += logMsg;
 	}
 
-	public static function prettyPrint(text:String) {
+	public static function prettyPrint(text:String)
+	{
 		var lines = text.split("\n");
 		var length = -1;
-		for (line in lines) if(line.length > length) length = line.length;
-					  
+		for (line in lines)
+			if (line.length > length)
+				length = line.length;
+
 		var header = "======";
-		for (i in 0...length) header += "=";
+		for (i in 0...length)
+			header += "=";
 
 		Sys.println("");
 		Sys.println('|$header|');
-		for (line in lines) {
-			Sys.println('|   ${ansiColor(RESET, CYAN)+centerText(line, length)+ansiColor(RESET, WHITE)}   |');
+		for (line in lines)
+		{
+			Sys.println('|   ${ansi(RESET, CYAN) + centerText(line, length) + ansi(RESET, WHITE)}   |');
 		}
 		Sys.println('|$header|');
 	}
 
-	public static function centerText(text:String, width:Int):String {
+	public static function centerText(text:String, width:Int):String
+	{
 		var centerOffset = (width - text.length) / 2;
 		var left = repeat(' ', Math.floor(centerOffset));
 		var right = repeat(' ', Math.ceil(centerOffset));
 		return left + text + right;
 	}
 
-	public static inline function repeat(ch:String, amt:Int) {
+	public static inline function repeat(ch:String, amt:Int)
+	{
 		var str = "";
-		for(i in 0...amt)
+		for (i in 0...amt)
 			str += ch;
 		return str;
 	}
